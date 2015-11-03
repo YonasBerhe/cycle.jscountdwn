3a2cb4c16952f5ecac3ca8065703a6898cde8407
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-11-02 21:03:30 -0800

    intial commit

---
+++ babel.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var _rx = require('rx');
+
+var _rx2 = _interopRequireDefault(_rx);
+
+var _core = require('@cycle/core');
+
+var _core2 = _interopRequireDefault(_core);
+
+var _dom = require('@cycle/dom');
+
+var _dom2 = _interopRequireDefault(_dom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function main() {
+  return {
+    DOM: _rx2.default.Observable.interval(1000).map(i => _dom2.default.h('h1', '' + i + ' seconds elapsed'))
+  };
+}
+
+let drivers = {
+  DOM: _dom2.default.makeDOMDriver('#app')
+};
+
+_core2.default.run(main, drivers);
---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+import Rx from 'rx';
+import Cycle from '@cycle/core';
+import CycleDOM from '@cycle/dom';
+
+function main() {
+  return {
+    DOM: Rx.Observable.interval(1000)
+      .map(i => CycleDOM.h(
+        'h1', '' + i + ' seconds elapsed'
+      ))
+  };
+}
+
+let drivers = {
+  DOM: CycleDOM.makeDOMDriver('#app')
+};
+
+Cycle.run(main, drivers);
